a3a4925391954be54c0e04c31c5a3eb7553abc1c
EndrisMoh <endrismd@gmail.com>
2023-02-16 05:12:29 -0800

    Objects update

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -20,10 +20,10 @@ def places_amenities(place_id):
         abort(404)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        l = [amenity.to_dict() for amenity in place.amenities]
+        lc = [amenity.to_dict() for amenity in place.amenities]
     else:
-        l = [storage.get("Amenity", id).to_dict() for id in place.amenity_ids]
-    return jsonify(l)
+        lc = [storage.get("Amenity", id).to_dict() for id in place.amenity_ids]
+    return jsonify(lc)
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
